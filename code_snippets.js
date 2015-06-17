a524efea0b96e57809bdf8931c465866c397e1dd
scofin73 <scofin73@yahoo.com>
2015-06-16 21:32:57 -0500

    first commit

---
+++ code_snippets.js
@@ -0,0 +1,41 @@
+
+  $('#painting1').data({id:1, price 250});
+  $('#painting2').data({id:2, price 400});
+  $('#painting3').data({id:3, price 75});
+  $('#painting4').data({id:4, price 150});
+  $('#painting5').data({id:5, price 200});
+  $('#painting6').data({id:6, price 350});
+  
+  $(document).ready(function(){
+      var theValue;
+	  
+	  $("#slider").change(function(){
+	      theValue = $("#rangeValue").val();
+		  filterItems(theValue);
+		  
+	  });
+	 
+	});
+	
+	function filterItems(priceCriteria)
+	{
+	
+	     $.each($('#gallery div'), function (i, item){
+		   $item = $(item);
+		   itemData = $item.data();
+		   if(itemData.price <= priceCriteria)
+     }
+	 
+	 else 
+	 {
+	 
+	       $item.animate({opacity:0.5});
+		   itemData.matching = false
+		   
+	 }
+  });
+  
+ }
+	 
+	    
+
